31f2d21b2e27cd274fab279902a4cba5a28c068c
paulohg6 <paulo_smr1@hotmail.com>
2021-12-29 20:23:09 -0300

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,6 +5,7 @@ test('basic test', async ({ page }) => {
   await page.fill('[id="email"]', 'paulo_smr1@hotmail.com')
   await page.fill('[id="password"]', '32106254')
   await page.click('[data-testid="entrar"]')
+  
   await expect((await page.innerText('[data-testid="logout"]'))).toEqual('Logout')
 
 
